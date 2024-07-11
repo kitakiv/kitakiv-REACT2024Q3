394c8620fe13a2a6f0b950b26a2dd80216b138db
Viktoriya Nykytenko <kitakiv@gmail.com>
2024-07-11 13:54:23 +0300

    feat: make class Search function

--- starWars/src/components/search.tsx
+++ starWars/src/components/search.tsx
@@ -1,73 +1,50 @@
-import React, { ChangeEvent, FormEvent } from 'react';
+import { ChangeEvent, FormEvent, useState, useEffect } from 'react';
 
-interface MyComponentState {
-  inputValue: string;
-  errorInput: string;
-}
+function Search(props: { onSearch: (search: string) => void }) {
+  const [inputValue, setInputValue] = useState('');
+  const [errorInput, setErrorInput] = useState('');
 
-class Search extends React.Component<
-  { onSearch: (search: string) => void },
-  MyComponentState
-> {
-  constructor(props: { onSearch: (search: string) => void }) {
-    super(props);
-    this.state = {
-      inputValue: '',
-      errorInput: '',
-    };
-    this.submitResult = this.submitResult.bind(this);
-    this.handleNameChange = this.handleNameChange.bind(this);
-  }
+  useEffect(() => {
+    setInputValue(localStorage.getItem('search') || '');
+  }, []);
 
-  componentDidMount(): void {
-    this.setState({ inputValue: localStorage.getItem('search') || '' });
-  }
-
-  submitResult(event: FormEvent<HTMLFormElement>) {
+  function submitResult(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    if (
-      !this.state.inputValue.startsWith(' ') &&
-      !this.state.inputValue.endsWith(' ')
-    ) {
-      this.props.onSearch(this.state.inputValue);
+    if (!inputValue.startsWith(' ') && !inputValue.endsWith(' ')) {
+      props.onSearch(inputValue);
     }
   }
 
-  handleNameChange(event: ChangeEvent<HTMLInputElement>) {
+  function handleNameChange(event: ChangeEvent<HTMLInputElement>) {
     const { value } = event.target as HTMLInputElement;
-    this.setState({ inputValue: value });
+    setInputValue(value);
     if (value.startsWith(' ') || value.endsWith(' ')) {
-      this.setState({
-        errorInput: 'Please remove space from starting and ending of the name',
-      });
+      setErrorInput('Please remove space from starting and ending of the name');
     } else {
-      this.setState({ errorInput: '' });
+      setErrorInput('');
     }
   }
-
-  render() {
-    return (
-      <>
-        <section className="app-search">
-          <form onSubmit={this.submitResult}>
-            <div>
-              <input
-                type="text"
-                name="search"
-                className="shadow"
-                value={this.state.inputValue}
-                onChange={this.handleNameChange}
-                id="search"
-                placeholder="Darth Vader"
-              />
-              <label htmlFor="search">{this.state.errorInput}</label>
-            </div>
-            <button type="submit"></button>
-          </form>
-        </section>
-      </>
-    );
-  }
+  return (
+    <>
+      <section className="app-search">
+        <form onSubmit={submitResult}>
+          <div>
+            <input
+              type="text"
+              name="search"
+              className="shadow"
+              value={inputValue}
+              onChange={handleNameChange}
+              id="search"
+              placeholder="Darth Vader"
+            />
+            <label htmlFor="search">{errorInput}</label>
+          </div>
+          <button type="submit"></button>
+        </form>
+      </section>
+    </>
+  );
 }
 
 export default Search;
