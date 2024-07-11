3a74f9104817053c081cbb6aad1ddfb146d64257
Viktoriya Nykytenko <kitakiv@gmail.com>
2024-07-11 13:47:59 +0300

    feat: make class SearchResults function

--- starWars/src/components/searchResults.tsx
+++ starWars/src/components/searchResults.tsx
@@ -1,53 +1,32 @@
-import React from 'react';
+import { useState } from 'react';
 import { SWApiResponse, SWFilm } from '../interface/interface';
 import Result from './result';
 
-interface State {
-  errorText: string;
-  error: boolean;
-}
+function Results(props: {
+  result: SWApiResponse | null;
+  filmRes: SWFilm[] | null;
+}) {
+  const [error, setError] = useState(false);
+  const errorText = 'Something went wrong. Please try again later.';
 
-class Results extends React.Component<
-  {
-    result: SWApiResponse | null;
-    filmRes: null | SWFilm[];
-  },
-  State
-> {
-  constructor(props: {
-    result: SWApiResponse | null;
-    filmRes: SWFilm[] | null;
-  }) {
-    super(props);
-
-    this.state = {
-      errorText: 'Something went wrong. Please try again later.',
-      error: false,
-    };
+  if (error) {
+    throw new Error(errorText);
   }
-  render() {
-    if (this.state.error) {
-      throw new Error(this.state.errorText);
-    }
 
-    return (
-      <section>
-        <button
-          className="error-button shadow"
-          onClick={() => this.setState({ error: true })}
-        >
-          Throw an error
-        </button>
-        {this.props.result !== null && this.props.filmRes !== null && (
-          <div className="results">
-            {...this.props.result.results.map((elem) => {
-              return <Result result={elem} filmRes={this.props.filmRes} />;
-            })}
-          </div>
-        )}
-      </section>
-    );
-  }
+  return (
+    <section>
+      <button className="error-button shadow" onClick={() => setError(true)}>
+        Throw an error
+      </button>
+      {props.result !== null && props.filmRes !== null && (
+        <div className="results">
+          {...props.result.results.map((elem) => {
+            return <Result result={elem} filmRes={props.filmRes} />;
+          })}
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default Results;
