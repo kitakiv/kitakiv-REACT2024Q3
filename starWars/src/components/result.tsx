9292be737b8f33aa18fd54f2fa0ef105fead5e58
Viktoriya Nykytenko <kitakiv@gmail.com>
2024-07-11 14:00:09 +0300

    feat: make class Result function

--- starWars/src/components/result.tsx
+++ starWars/src/components/result.tsx
@@ -5,53 +5,31 @@ interface Props {
   filmRes: SWFilm[] | null;
 }
 
-class Result extends React.Component<Props> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      'https://swapi.dev/api/films/1/': this.props.filmRes
-        ? this.props.filmRes[0].title
-        : '1',
-      'https://swapi.dev/api/films/2/': this.props.filmRes
-        ? this.props.filmRes[1].title
-        : '2',
-      'https://swapi.dev/api/films/3/': this.props.filmRes
-        ? this.props.filmRes[2].title
-        : '3',
-      'https://swapi.dev/api/films/4/': this.props.filmRes
-        ? this.props.filmRes[3].title
-        : '4',
-      'https://swapi.dev/api/films/5/': this.props.filmRes
-        ? this.props.filmRes[4].title
-        : '5',
-      'https://swapi.dev/api/films/6/': this.props.filmRes
-        ? this.props.filmRes[5].title
-        : '6',
-    };
-  }
-  render() {
-    return (
-      <div className="results-hero shadow">
-        <h3>{this.props.result.name}</h3>
-        <ul>
-          <li>Height: {this.props.result.height}</li>
-          <li>Mass: {this.props.result.mass}</li>
-          <li>Hair color: {this.props.result.hair_color}</li>
-          <li>Skin color: {this.props.result.skin_color}</li>
-          <li>Eye color: {this.props.result.eye_color}</li>
-          <li>Birth year: {this.props.result.birth_year}</li>
-          <li>Gender: {this.props.result.gender}</li>
-        </ul>
-        <h4>Films</h4>
-        <ul>
-          {...this.props.result.films.map((elem) => {
-            const key = elem;
-            return <li>{this.state[key as keyof typeof this.state]}</li>;
-          })}
-        </ul>
-      </div>
-    );
-  }
-}
+const Result: React.FC<Props> = ({ result, filmRes }) => {
+  const getFilmTitle = (filmUrl: string) => {
+    return filmRes?.find((film) => film.url === filmUrl)?.title || 'Unknown';
+  };
+
+  return (
+    <div className="results-hero shadow">
+      <h3>{result.name}</h3>
+      <ul>
+        <li>Height: {result.height}</li>
+        <li>Mass: {result.mass}</li>
+        <li>Hair color: {result.hair_color}</li>
+        <li>Skin color: {result.skin_color}</li>
+        <li>Eye color: {result.eye_color}</li>
+        <li>Birth year: {result.birth_year}</li>
+        <li>Gender: {result.gender}</li>
+      </ul>
+      <h4>Films</h4>
+      <ul>
+        {result.films.map((filmUrl) => (
+          <li key={filmUrl}>{getFilmTitle(filmUrl)}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Result;
