29a436a80e9be0e067180a2ddd61f80061924bdd
Viktoriya Nykytenko <kitakiv@gmail.com>
2024-07-11 14:01:27 +0300

    feat: make class Loader function

--- starWars/src/components/loader.tsx
+++ starWars/src/components/loader.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-
-class Loader extends React.Component {
-  render() {
-    return (
-      <div className="pop-up-load">
-        <div className="loader"></div>
-      </div>
-    );
-  }
+function Loader() {
+  return (
+    <div className="pop-up-load">
+      <div className="loader"></div>
+    </div>
+  );
 }
 
 export default Loader;
