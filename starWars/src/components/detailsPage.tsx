4b94e1d5ba043c84ddc337e73de481b0a8045bed
Viktoriya Nykytenko <kitakiv@gmail.com>
2024-07-13 18:37:13 +0300

    feat: add loader

--- starWars/src/components/detailsPage.tsx
+++ starWars/src/components/detailsPage.tsx
@@ -1,7 +1,14 @@
-import { useLoaderData, Link, useParams } from 'react-router-dom';
+import {
+  useLoaderData,
+  Link,
+  useParams,
+  useNavigation,
+} from 'react-router-dom';
 import { SWCharacter } from '../interface/interface';
+import Loader from './loader';
 
 function DetailsPage() {
+  const { state } = useNavigation();
   const [data, films, homeworld, vehicles, starships] = useLoaderData() as [
     SWCharacter,
     string[],
@@ -12,63 +19,66 @@ function DetailsPage() {
   const { search, page } = useParams();
 
   return (
-    <div className="details-page">
-      <div>
-        <h1>{data.name}</h1>
-        <img src="/logo.svg" alt="vader" className="vader" />
-      </div>
-      <div>
-        <h3>Details</h3>
-        <ul>
-          <li>Height: {data.height}</li>
-          <li>Mass: {data.mass}</li>
-          <li>Hair color: {data.hair_color}</li>
-          <li>Skin color: {data.skin_color}</li>
-          <li>Eye color: {data.eye_color}</li>
-          <li>Birth year: {data.birth_year}</li>
-          <li>Gender: {data.gender}</li>
-        </ul>
-      </div>
-      <div>
-        <h3>Films</h3>
-        <ul>
-          {...films.map((film) => (
-            <>
-              <li>{film}</li>
-            </>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <h3>Homeworld</h3>
-        <p>{homeworld}</p>
-      </div>
-      {vehicles.length > 0 && (
+    <>
+      {state === 'loading' && <Loader />}
+      <div className="details-page">
         <div>
-          <h3>Vehicles</h3>
+          <h1>{data.name}</h1>
+          <img src="/logo.svg" alt="vader" className="vader" />
+        </div>
+        <div>
+          <h3>Details</h3>
           <ul>
-            {...vehicles.map((vehicle) => (
+            <li>Height: {data.height}</li>
+            <li>Mass: {data.mass}</li>
+            <li>Hair color: {data.hair_color}</li>
+            <li>Skin color: {data.skin_color}</li>
+            <li>Eye color: {data.eye_color}</li>
+            <li>Birth year: {data.birth_year}</li>
+            <li>Gender: {data.gender}</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Films</h3>
+          <ul>
+            {...films.map((film) => (
               <>
-                <li>{vehicle}</li>
+                <li>{film}</li>
               </>
             ))}
           </ul>
         </div>
-      )}
-      {starships.length > 0 && (
         <div>
-          <h3>Starships</h3>
-          <ul>
-            {...starships.map((starship) => (
-              <>
-                <li>{starship}</li>
-              </>
-            ))}
-          </ul>
+          <h3>Homeworld</h3>
+          <p>{homeworld}</p>
         </div>
-      )}
-      <Link to={`/search/${search}/page/${page}`} className="cross"></Link>
-    </div>
+        {vehicles.length > 0 && (
+          <div>
+            <h3>Vehicles</h3>
+            <ul>
+              {...vehicles.map((vehicle) => (
+                <>
+                  <li>{vehicle}</li>
+                </>
+              ))}
+            </ul>
+          </div>
+        )}
+        {starships.length > 0 && (
+          <div>
+            <h3>Starships</h3>
+            <ul>
+              {...starships.map((starship) => (
+                <>
+                  <li>{starship}</li>
+                </>
+              ))}
+            </ul>
+          </div>
+        )}
+        <Link to={`/search/${search}/page/${page}`} className="cross"></Link>
+      </div>
+    </>
   );
 }
 
--- starWars/src/components/searchResults.tsx
+++ starWars/src/components/searchResults.tsx
@@ -1,11 +1,19 @@
 import { useState } from 'react';
 import { SWApiResponse } from '../interface/interface';
 import Result from './result';
-import { useLoaderData, Outlet, useParams, Link } from 'react-router-dom';
+import {
+  useLoaderData,
+  Outlet,
+  useParams,
+  Link,
+  useNavigation,
+} from 'react-router-dom';
 import NoResults from './noResults';
+import Loader from './loader';
 
 function Results() {
   const [result] = useLoaderData() as [SWApiResponse];
+  const { state } = useNavigation();
   const [error, setError] = useState(false);
   const errorText = 'Something went wrong. Please try again later.';
   const { search, page, id } = useParams();
@@ -15,60 +23,66 @@ function Results() {
   }
 
   return (
-    <section className="app-results">
-      <>
-        <button className="error-button shadow" onClick={() => setError(true)}>
-          Throw an error
-        </button>
-        {result !== null && result.count > 0 && (
-          <div className="results-container">
-            <div className="results">
-              {...result.results.map((elem) => {
-                const number = elem.url;
-                return <Result result={elem} keyProps={number} />;
-              })}
+    <>
+      {state === 'loading' && <Loader />}
+      <section className="app-results">
+        <>
+          <button
+            className="error-button shadow"
+            onClick={() => setError(true)}
+          >
+            Throw an error
+          </button>
+          {result !== null && result.count > 0 && (
+            <div className="results-container">
+              <div className="results">
+                {...result.results.map((elem) => {
+                  const number = elem.url;
+                  return <Result result={elem} keyProps={number} />;
+                })}
+              </div>
+              <div className="pagination">
+                {result.previous !== null && id !== undefined && (
+                  <Link
+                    className="link-page"
+                    to={`/search/${search}/page/${(result.previous as string).split('page=')[1]}/detail/${id}`}
+                  >
+                    {(result.previous as string).split('page=')[1]}
+                  </Link>
+                )}
+                {result.previous !== null && id === undefined && (
+                  <Link
+                    className="link-page"
+                    to={`/search/${search}/page/${(result.previous as string).split('page=')[1]}`}
+                  >
+                    {(result.previous as string).split('page=')[1]}
+                  </Link>
+                )}
+                <div className="link-page link-page-active">{page}</div>
+                {result.next !== null && id !== undefined && (
+                  <Link
+                    className="link-page"
+                    to={`/search/${search}/page/${(result.next as string).split('page=')[1]}/detail/${id}`}
+                  >
+                    {(result.next as string).split('page=')[1]}
+                  </Link>
+                )}
+                {result.next !== null && id === undefined && (
+                  <Link
+                    className="link-page"
+                    to={`/search/${search}/page/${(result.next as string).split('page=')[1]}`}
+                  >
+                    {(result.next as string).split('page=')[1]}
+                  </Link>
+                )}
+              </div>
             </div>
-            <div className="pagination">
-              {result.previous !== null && id !== undefined && (
-                <Link
-                  className="link-page"
-                  to={`/search/${search}/page/${(result.previous as string).split('page=')[1]}/detail/${id}`}
-                >
-                  {(result.previous as string).split('page=')[1]}
-                </Link>
-              )}
-              {result.previous !== null && id === undefined && (
-                <Link
-                  className="link-page"
-                  to={`/search/${search}/page/${(result.previous as string).split('page=')[1]}`}
-                >
-                  {(result.previous as string).split('page=')[1]}
-                </Link>
-              )}
-              <div className="link-page link-page-active">{page}</div>
-              {result.next !== null && id !== undefined && (
-                <Link
-                  className="link-page"
-                  to={`/search/${search}/page/${(result.next as string).split('page=')[1]}/detail/${id}`}
-                >
-                  {(result.next as string).split('page=')[1]}
-                </Link>
-              )}
-              {result.next !== null && id === undefined && (
-                <Link
-                  className="link-page"
-                  to={`/search/${search}/page/${(result.next as string).split('page=')[1]}`}
-                >
-                  {(result.next as string).split('page=')[1]}
-                </Link>
-              )}
-            </div>
-          </div>
-        )}
-        {result !== null && result.count === 0 && <NoResults />}
-      </>
-      <Outlet />
-    </section>
+          )}
+          {result !== null && result.count === 0 && <NoResults />}
+        </>
+        <Outlet />
+      </section>
+    </>
   );
 }
 
