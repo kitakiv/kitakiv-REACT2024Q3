bd056bf3d31eae0befba7b333e6129e11570140c
Viktoriya Nykytenko <kitakiv@gmail.com>
2024-08-05 13:44:41 +0300

    feat: make right routes

--- starWars/features/details/detailsPage.tsx
+++ starWars/features/details/detailsPage.tsx
@@ -1,6 +1,7 @@
 import { SWCharacter } from '../../interface/interface';
 import Link from 'next/link';
 import React from 'react';
+import NoResults from '../../components/noResults';
 
 function DetailsPage({
   details,
@@ -15,38 +16,42 @@ function DetailsPage({
 }) {
   return (
     <>
-      <div className="details-page">
-        <div>
-          <h1>{details.name}</h1>
-          <div className="vader"></div>
+      {details.films ? (
+        <div className="details-page">
+          <div>
+            <h1>{details.name}</h1>
+            <div className="vader"></div>
+          </div>
+          <div>
+            <h3>Details</h3>
+            <ul>
+              <li>Height: {details.height}</li>
+              <li>Mass: {details.mass}</li>
+              <li>Hair color: {details.hair_color}</li>
+              <li>Skin color: {details.skin_color}</li>
+              <li>Eye color: {details.eye_color}</li>
+              <li>Birth year: {details.birth_year}</li>
+              <li>Gender: {details.gender}</li>
+            </ul>
+          </div>
+          <div className="films" id="films">
+            <h3>Films</h3>
+            <ul>
+              {details.films.map((filmLink) => (
+                <li key={filmLink}>{filmsResult[filmLink]}</li>
+              ))}
+            </ul>
+          </div>
+          <div></div>
+          <Link
+            href={`/?search=${search}&page=${page}`}
+            className="cross"
+            defaultValue="back"
+          ></Link>
         </div>
-        <div>
-          <h3>Details</h3>
-          <ul>
-            <li>Height: {details.height}</li>
-            <li>Mass: {details.mass}</li>
-            <li>Hair color: {details.hair_color}</li>
-            <li>Skin color: {details.skin_color}</li>
-            <li>Eye color: {details.eye_color}</li>
-            <li>Birth year: {details.birth_year}</li>
-            <li>Gender: {details.gender}</li>
-          </ul>
-        </div>
-        <div className="films" id="films">
-          <h3>Films</h3>
-          <ul>
-            {details.films.map((filmLink) => (
-              <li key={filmLink}>{filmsResult[filmLink]}</li>
-            ))}
-          </ul>
-        </div>
-        <div></div>
-        <Link
-          href={`/?search=${search}&page=${page}`}
-          className="cross"
-          defaultValue="back"
-        ></Link>
-      </div>
+      ) : (
+        <NoResults />
+      )}
     </>
   );
 }
